c814b455f7cda6df237afa04f2e8077b94b143ad
Daniël Schenk <danielschenk@gmail.com>
2024-05-20 22:47:36 +0200

    Work in progress: on/off detection

--- components/intex_sf90220rc1/intex_sf90220rc1.cpp
+++ components/intex_sf90220rc1/intex_sf90220rc1.cpp
@@ -1,12 +1,25 @@
+#include "intex_sf90220rc1.h"
 #include "esphome/core/log.h"
 #include "esphome/core/time.h"
-#include "intex_sf90220rc1.h"
+#include "esphome/components/switch/switch.h"
 
 namespace esphome {
 namespace intex_sf90220rc1 {
 
 namespace messages {
   static constexpr std::array<uint8_t, 4> kPowerPressed{0x02, 0xFD, 0x04, 0x00};
+
+  static constexpr std::array<uint8_t, 2> kDisplayHeader{0x00, 0xFF};
+  static constexpr size_t kMinReceiveSize = 4;
+  static constexpr size_t kDisplayByteOffset = 2;
+  enum DisplayValue : uint8_t {
+    kBlank = 0xBB,
+    kDot = 0xAB,
+  };
+
+  static constexpr uint32_t kBaudRate = 2400;
+  static constexpr uint32_t kCharacterBits = 10; // 8N1: 8 data bits, no parity, 1 start and 1 stop bit
+  static constexpr uint32_t kCharacterTimeoutMillis = 1000 * (1.5 * kCharacterBits / kBaudRate);
 } //namespace messages
 
 static const char *TAG = "intex_sf90220rc1";
@@ -21,10 +34,53 @@ void IntexSF90220RC1::loop() {
     this->write_array(messages::kPowerPressed);
     this->last_write_ = now;
   }
+
+  process_rx();
+}
+
+void IntexSF90220RC1::process_rx() {
+  uint32_t now = millis();
+
+  while (this->available()) {
+    uint8_t byte;
+    this->read_byte(&byte);
+    if (this->receiving_) {
+      this->rx_msg_.push_back(byte);
+    }
+    this->last_rx_byte_time_ = now;
+  }
+
+  if (now - this->last_rx_byte_time_ >= messages::kCharacterTimeoutMillis) {
+    this->receiving_ = true;
+  } else {
+    return;
+  }
+
+  process_msg();
+}
+
+void IntexSF90220RC1::process_msg() {
+  if (this->rx_msg_.size() < messages::kMinReceiveSize) {
+    ESP_LOGD(TAG, "Received invalid frame: insufficient size");
+    this->rx_msg_.clear();
+    return;
+  }
+
+  if (this->rx_msg_[0] != messages::kDisplayHeader[0] ||
+      this->rx_msg_[1] != messages::kDisplayHeader[1]) {
+    ESP_LOGD(TAG, "Received invalid frame: unexpected header");
+    this->rx_msg_.clear();
+    return;
+  }
+
+  uint8_t displayByte = this->rx_msg_[messages::kDisplayByteOffset];
+  this->power_switch_->publish_state(displayByte != messages::DisplayValue::kDot);
+
+  this->rx_msg_.clear();
 }
 
 void IntexSF90220RC1::dump_config() {
-    ESP_LOGCONFIG(TAG, "Empty compound sensor");
+    ESP_LOGCONFIG(TAG, "Intex SF90220RC1 filter pump");
 }
 
 } //namespace intex_sf90220rc1
--- components/intex_sf90220rc1/intex_sf90220rc1.h
+++ components/intex_sf90220rc1/intex_sf90220rc1.h
@@ -4,6 +4,10 @@
 #include "esphome/components/uart/uart.h"
 #include <array>
 #include <cstdint>
+#include <vector>
+namespace switch_ {
+class Switch;
+}
 
 namespace esphome {
 namespace intex_sf90220rc1 {
@@ -15,8 +19,21 @@ class IntexSF90220RC1 : public Component, public uart::UARTDevice {
     void loop() override;
     void dump_config() override;
 
+    void set_power_switch(switch_::Switch *power_switch) { this->power_switch_ = power_switch; }
+
   protected:
+    void process_rx();
+    void process_msg();
+
     uint32_t last_write_{0};
+
+    // discard at boot until frame timeout (might have woken up in the middle of a frame)
+    bool receiving_{false};
+    uint32_t last_rx_byte_time_{0};
+    uint32_t last_rx_msg_time_{0};
+    std::vector<uint8_t> rx_msg_;
+
+    switch_::Switch *power_switch_{nullptr};
 };
 
 } //namespace intex_sf90220rc1
