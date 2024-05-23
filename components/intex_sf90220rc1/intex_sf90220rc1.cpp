f21c0e4ca09b6e0862d6579f64e8cae763e6d0b2
Daniël Schenk <danielschenk@gmail.com>
2024-05-23 23:49:59 +0200

    Make on/off detection work robustly

--- components/intex_sf90220rc1/intex_sf90220rc1.cpp
+++ components/intex_sf90220rc1/intex_sf90220rc1.cpp
@@ -11,7 +11,6 @@ namespace messages {
 
   static constexpr std::array<uint8_t, 2> kDisplayHeader{0x00, 0xFF};
   static constexpr size_t kMinReceiveSize = 4;
-  static constexpr size_t kDisplayByteOffset = 2;
   enum DisplayValue : uint8_t {
     kBlank = 0xBB,
     kDot = 0xAB,
@@ -20,6 +19,8 @@ namespace messages {
   static constexpr uint32_t kBaudRate = 2400;
   static constexpr uint32_t kCharacterBits = 10; // 8N1: 8 data bits, no parity, 1 start and 1 stop bit
   static constexpr uint32_t kCharacterTimeoutMillis = 1000 * (1.5 * kCharacterBits / kBaudRate);
+
+  static constexpr uint32_t kDisplayMessageTimeoutMillis = 1000;
 } //namespace messages
 
 static const char *TAG = "intex_sf90220rc1";
@@ -30,6 +31,7 @@ void IntexSF90220RC1::setup() {
 
 void IntexSF90220RC1::loop() {
   uint32_t now = millis();
+  // for now, auto-toggle as a test
   if (now - this->last_write_ > 2000) {
     this->write_array(messages::kPowerPressed);
     this->last_write_ = now;
@@ -40,12 +42,15 @@ void IntexSF90220RC1::loop() {
 
 void IntexSF90220RC1::process_rx() {
   uint32_t now = millis();
+  if (now - this->last_rx_msg_time_ >= messages::kDisplayMessageTimeoutMillis) {
+    update_power_state(false, "display message timeout");
+  }
 
   while (this->available()) {
     uint8_t byte;
     this->read_byte(&byte);
     if (this->receiving_) {
-      this->rx_msg_.push_back(byte);
+      this->rx_buf_.push_back(byte);
     }
     this->last_rx_byte_time_ = now;
   }
@@ -56,7 +61,7 @@ void IntexSF90220RC1::process_rx() {
     return;
   }
 
-  if (this->rx_msg_.empty()) {
+  if (this->rx_buf_.empty()) {
     return;
   }
 
@@ -64,23 +69,53 @@ void IntexSF90220RC1::process_rx() {
 }
 
 void IntexSF90220RC1::process_msg() {
-  if (this->rx_msg_.size() < messages::kMinReceiveSize) {
+  size_t bytes_avail = this->rx_buf_.size();
+  if (bytes_avail < messages::kMinReceiveSize) {
     ESP_LOGD(TAG, "Received invalid frame: insufficient size");
-    this->rx_msg_.clear();
+    this->rx_buf_.clear();
     return;
   }
 
-  if (this->rx_msg_[0] != messages::kDisplayHeader[0] ||
-      this->rx_msg_[1] != messages::kDisplayHeader[1]) {
+  size_t num_msgs = bytes_avail / messages::kMinReceiveSize;
+  for (size_t msg = 0; msg < num_msgs; ++msg) {
+    uint8_t display_byte;
+    if (!this->read_display_msg(display_byte)) {
+      continue;
+    }
+    update_power_state(display_byte != messages::DisplayValue::kDot, "display message");
+  }
+
+  this->last_rx_msg_time_ = millis();
+}
+
+bool IntexSF90220RC1::read_display_msg(uint8_t& display_byte) {
+  uint8_t header1, header2;
+  header1 = this->rx_buf_.front();
+  this->rx_buf_.pop_front();
+  header2 = this->rx_buf_.front();
+  this->rx_buf_.pop_front();
+
+  if (header1 != messages::kDisplayHeader[0] ||
+      header2 != messages::kDisplayHeader[1]) {
     ESP_LOGD(TAG, "Received invalid frame: unexpected header");
-    this->rx_msg_.clear();
-    return;
+    this->rx_buf_.pop_front();
+    this->rx_buf_.pop_front();
+    return false;
   }
 
-  uint8_t displayByte = this->rx_msg_[messages::kDisplayByteOffset];
-  this->power_switch_->publish_state(displayByte != messages::DisplayValue::kDot);
+  display_byte = this->rx_buf_.front();
+  this->rx_buf_.pop_front();
+  this->rx_buf_.pop_front();
+  return true;
+}
 
-  this->rx_msg_.clear();
+void IntexSF90220RC1::update_power_state(bool state, const char* source) {
+  if (!this->power_state_known_ || this->last_power_state_ != state) {
+    this->power_switch_->publish_state(state);
+    ESP_LOGI(TAG, "Turned %s (%s)", state ? "on" : "off", source);
+    this->last_power_state_ = state;
+    this->power_state_known_ = true;
+  }
 }
 
 void IntexSF90220RC1::dump_config() {
--- components/intex_sf90220rc1/intex_sf90220rc1.h
+++ components/intex_sf90220rc1/intex_sf90220rc1.h
@@ -4,7 +4,7 @@
 #include "esphome/components/uart/uart.h"
 #include <array>
 #include <cstdint>
-#include <vector>
+#include <deque>
 
 namespace esphome {
 namespace switch_ {
@@ -25,6 +25,9 @@ class IntexSF90220RC1 : public Component, public uart::UARTDevice {
   protected:
     void process_rx();
     void process_msg();
+    bool read_display_msg(uint8_t& display_byte);
+
+    void update_power_state(bool state, const char* source);
 
     uint32_t last_write_{0};
 
@@ -32,8 +35,11 @@ class IntexSF90220RC1 : public Component, public uart::UARTDevice {
     bool receiving_{false};
     uint32_t last_rx_byte_time_{0};
     uint32_t last_rx_msg_time_{0};
-    std::vector<uint8_t> rx_msg_;
+    std::deque<uint8_t> rx_buf_;
 
+    // due to lack of std::optional support, track this in a separate var
+    bool power_state_known_{false};
+    bool last_power_state_;
     switch_::Switch *power_switch_{nullptr};
 };
 
