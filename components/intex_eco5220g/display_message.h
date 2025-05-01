040c2f0cf0a4e6bf8d4adf6c073c8a3737eb859c
Daniël Schenk <danielschenk@users.noreply.github.com>
2025-05-01 11:25:19 +0200

    Unit test infrastructure and CI update (#17)
    * WIP on message class and unit testing
    
    * Add unit tests to CI
    
    * Work around permission issue for test result action
    
    * debug ci issue
    
    * try without junit
    
    * try running ctest directly
    
    * fix issue
    
    * Initial display message decoding
    
    * update ESP actions in workflows
    
    * fix build error

---
+++ components/intex_eco5220g/display_message.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <cstddef>
+
+namespace esphome {
+namespace intex_eco5220g {
+
+class DisplayMessage {
+  public:
+    typedef std::array<uint8_t, 4> RawMessage;
+    DisplayMessage() = default;
+    DisplayMessage(const DisplayMessage& other) = default;
+    DisplayMessage& operator=(const DisplayMessage& other) = default;
+
+    DisplayMessage(const RawMessage& raw_message);
+
+    bool is_power_on_set() const;
+
+  protected:
+    static constexpr size_t kLedByteOffset = 2;
+    enum LedBit : uint8_t
+    {
+      kPowerOn = 0x02,
+    };
+
+    RawMessage raw_message_{0};
+};
+
+} //namespace intex_eco5220g
+} //namespace esphome
---
+++ components/intex_eco5220g/test/display_message_test.cpp
@@ -0,0 +1,15 @@
+#include "../display_message.h"
+#include <gtest/gtest.h>
+
+namespace esphome {
+namespace intex_eco5220g {
+
+TEST(DisplayMessageTest, IsPowerOnSet) {
+  DisplayMessage message;
+  EXPECT_FALSE(message.is_power_on_set());
+  DisplayMessage message2({0, 0, 0x02, 0});
+  EXPECT_TRUE(message2.is_power_on_set());
+}
+
+} // namespace intex_eco5220g
+} // namespace esphome
