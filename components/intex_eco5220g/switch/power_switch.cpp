4520f7836c79b45a5d3a562b00485031baf01a76
Daniël Schenk <danielschenk@gmail.com>
2025-05-15 12:06:45 +0200

    copy power switch code for now

--- components/intex_eco5220g/switch/power_switch.cpp
+++ components/intex_eco5220g/switch/power_switch.cpp
@@ -7,11 +7,30 @@
 namespace esphome {
 namespace intex_eco5220g {
 
-static const char *const TAG = "intexsf90220rc1.switch";
+static const char *const TAG = "intexeco5220g.switch";
 
-void PowerSwitch::dump_config() { LOG_SWITCH("", "Power Switch", this); }
+void PowerSwitch::dump_config() {
+  LOG_SWITCH("", "Power Switch", this);
+}
 
-void PowerSwitch::write_state(bool state) {}
+void PowerSwitch::write_state(bool state) {
+  if (this->parent_ == nullptr) {
+    ESP_LOGE(TAG, "Parent is null");
+    return;
+  }
+
+  auto is_power_on = this->parent_->is_power_on();
+  if (!is_power_on.has_value()) {
+    ESP_LOGE(TAG, "Power state is unknown, cannot toggle");
+    return;
+  }
+
+  if (*is_power_on != state) {
+    this->parent_->press_power();
+  } else {
+    ESP_LOGW(TAG, "Power state is already %s", state ? "on" : "off");
+  }
+}
 
 }  // namespace intex_eco5220g
 }  // namespace esphome
